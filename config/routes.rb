e896238bfcd33ab88948ceac5b7fc9bc3f41bf4f
bbjjki4 <takuma.hashimoto@logly.co.jp>
2019-05-28 17:58:21 +0900

    set rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'ad/index'
-  get 'ad/show'
-  get 'ad/edit'
-  get 'ad/new'
-  get 'ad/create'
-  get 'ad/edit'
-  get 'ad/destroy'
+  resources :ad
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
