83a34af929582fbc543a36a57ce867a467317c69
bbjjki4 <takuma.hashimoto@logly.co.jp>
2019-05-28 17:50:44 +0900

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'ad/index'
+  get 'ad/show'
+  get 'ad/edit'
+  get 'ad/new'
+  get 'ad/create'
+  get 'ad/edit'
+  get 'ad/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
