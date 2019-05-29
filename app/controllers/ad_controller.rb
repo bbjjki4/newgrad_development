a856f351c8438c83b7b2eb673d10d1bbfac5c1eb
bbjjki4 <takuma.hashimoto@logly.co.jp>
2019-05-29 13:17:15 +0900

    add action:destroy,update and flash

--- app/controllers/ad_controller.rb
+++ app/controllers/ad_controller.rb
@@ -7,6 +7,7 @@ class AdController < ApplicationController
   end
 
   def edit
+    @ad =Ad.find( params[:id])
   end
 
   def new
@@ -16,18 +17,26 @@ class AdController < ApplicationController
   def create
     @ad = Ad.new(ad_params)
     @ad.save
+    flash[:notice] = "Ad registered!"
     redirect_to("/ad")
   end
 
-  def edit
+  def update
+    @ad = Ad.find(params[:id])
+    @ad.update_attributes(ad_params)
+    flash[:notice] = "Ad updated!"
+    redirect_to("/ad")
   end
 
   def destroy
+    Ad.find(params[:id]).destroy
+    flash[:notice] = "Ad deleted!"
+    redirect_to("/ad")
   end
 
   private
 
   def ad_params
-    params.require(:ad).permit(:name,:text,:advertiser_id,:image)
+    params.require(:ad).permit(:price,:text,:advertiser_id,:image)
   end
 end
