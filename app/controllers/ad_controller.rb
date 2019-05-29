04046ae487d824c3fcdb85ba32388e2c7837d27e
bbjjki4 <takuma.hashimoto@logly.co.jp>
2019-05-29 10:32:16 +0900

    make new.erb and new,create action

--- app/controllers/ad_controller.rb
+++ app/controllers/ad_controller.rb
@@ -10,14 +10,24 @@ class AdController < ApplicationController
   end
 
   def new
+    @ad= Ad.new
   end
 
   def create
+    @ad = Ad.new(ad_params)
+    @ad.save
+    redirect_to("/ad")
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def ad_params
+    params.require(:ad).permit(:name,:text,:advertiser_id,:image)
   end
 end
