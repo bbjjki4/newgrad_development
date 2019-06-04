ec206610a2ef958d03486922a915f13c91979466
bbjjki4 <takuma.hashimoto@logly.co.jp>
2019-06-04 15:54:20 +0900

    make api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/view' => 'ad_api#view'
+  get '/click' => 'ad_api#click'
   resources :ad
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
